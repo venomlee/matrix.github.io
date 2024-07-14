8c67be8ed4b98ed4715f937fdd837c251c642cd5
venom <darksgrace@gmail.com>
2024-07-14 21:42:59 +0800

    increase font size.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
         //converting the string into an array of single characters
         matrix = matrix.split("");
 
-        var font_size = 5;
+        var font_size = 12;
         var columns = c.width/font_size; //number of columns for the rain
         //an array of drops - one per column
         var drops = [];
